5cbb0dcc8b9dd8660628fa1c6cdf11cf4af7ebbf
kamalogudah <mcpaul2058@gmail.com>
2018-05-26 15:37:46 +0300

    Working with modals

---
+++ modal/main.js
@@ -0,0 +1,29 @@
+Vue.component('modal', {
+
+  template: `
+  <div class="modal is-active">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+  <div class="box">
+     <p>
+       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+       text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+     </p>
+  </div>
+    </div>
+  <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+</div>
+
+  `
+
+})
+
+new Vue({
+  el: '#root',
+
+  data: {
+    showModal: false
+
+  }
+}
+)
