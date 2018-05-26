5fe9e06f3888e8c7dfa6b22c801b51bfe900b8cc
kamalogudah <mcpaul2058@gmail.com>
2018-05-26 13:43:23 +0300

    Creating a single list entry point

--- components/main.js
+++ components/main.js
@@ -1,5 +1,28 @@
-Vue.component('task',{
+Vue.component('task-list', {
+  template: `
+      <div>
+      <task v-for="task in tasks" v-text="task.task"></task>
+    </div>
+    `,
+
+  data(){
+    return {
+      tasks: [
+           {task: 'Go to the Store', complete: true},
+           {task: 'Go to the Store', complete: false},
+           {task: 'Go to the Store', complete: true},
+           {task: 'Go to the Store', complete: false}
+      ]
+
+    };
+
+  }
+});
+
+Vue.component('task', {
+
   template: `<li><slot></slot></li>`
+
 });
 
 new Vue({
